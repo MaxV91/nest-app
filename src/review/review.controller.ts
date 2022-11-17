c9ab01f361142269550dbd9989e7ca9bb8aeecee
Maxim K <max.kotsurenko@gmail.com>
2022-11-17 17:00:41 +0300

    user-email decorator

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -14,6 +14,7 @@ import { CreateReviewDto } from "./dto/create-review.dto";
 import { ReviewService } from "./review.service";
 import { REVIEW_NOT_FOUND } from "./review.constants";
 import { JwtAuthGuard } from "../auth/guards/jwt.guard";
+import { UserEmail } from "../decorators/user-email.decorator";
 
 @Controller("review")
 export class ReviewController {
@@ -36,7 +37,8 @@ export class ReviewController {
   }
 
   @Get("byProduct/:productId")
-  async getByProduct(@Param("productId") productId: string) {
+  async getByProduct(@Param("productId") productId: string, @UserEmail() email: string) {
+    console.log(email);
     return this.reviewService.findByProductId(productId);
   }
 }
