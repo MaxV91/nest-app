d63bbc4b0e2bb595ecd8a6f8c36f1cb77379608a
Maxim K <max.kotsurenko@gmail.com>
2022-11-23 19:17:22 +0300

    cron

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -19,12 +19,14 @@ import { NOT_FOUND_TOP_PAGE_ERROR } from "./top-page.constants";
 import { JwtAuthGuard } from "../auth/guards/jwt.guard";
 import { HhService } from "../hh/hh.service";
 import { rejects } from "assert";
+import { Cron, CronExpression, SchedulerRegistry } from "@nestjs/schedule";
 
 @Controller('top-page')
 export class TopPageController {
   constructor(
     private readonly topPageService: TopPageService,
-    private readonly hhService: HhService
+    private readonly hhService: HhService,
+    private readonly scheduleRegistry: SchedulerRegistry
   ) {
   }
 
@@ -83,8 +85,9 @@ export class TopPageController {
     return this.topPageService.findByText(text);
   }
 
-  @Post('test')
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, { name: 'test' })
   async test() {
+    const job = this.scheduleRegistry.getCronJob('test');
     const data = await this.topPageService.findForHhUpdate(new Date());
     for (let page of data) {
       const hhData = await this.hhService.getData(page.category);
